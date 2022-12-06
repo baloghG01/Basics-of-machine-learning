a7a60ad6e65969afcbe987b6529d7ae1fe93804a
baloghG01 <busnes14@gmail.com>
2022-12-06 18:52:03 +0100

    Decision tree

--- BasicsOfMachineLearning.py
+++ BasicsOfMachineLearning.py
@@ -2,6 +2,7 @@ import numpy as np  # importing numerical computing package
 import pandas as pd  # importing pandas data analysis tool
 from matplotlib import pyplot as plt # importing MATLAB-like plotting framework
 from sklearn.model_selection import train_test_split # importing data splitter
+from sklearn.tree import DecisionTreeClassifier 
 
  
 data =  np.loadtxt(fname = 'labor_exercise_wednesday1.csv', delimiter = ',') # Load dataset with numpy, it contains 10 input and 1 binary variables and doesn't have attributes names
@@ -30,6 +31,9 @@ plt.show()
 
 X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.3, random_state=2022) # Split the data set with a 30% test size
 
+class_tree = DecisionTreeClassifier(criterion = 'gini',max_depth = 4) # The firts model we use
+class_tree.fit(X_train, y_train) # Give the model the dataset
+class_tree_score_train= class_tree.score(X_train,y_train)
+class_tree_score_test = class_tree.score(X_test,y_test)
 
-
-
+print(class_tree_score_test)
